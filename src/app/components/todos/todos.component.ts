2e6baf286311e3a177c21bc9822144481b122450
Muhammet Emin Aynacı <75997729+braynaci24@users.noreply.github.com>
2023-04-24 23:34:14 +0300

    Angular Todo List

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,46 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Todo } from './../../models/Todo';
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+  todos: Todo[];
+  inputTodo: string = "";
+  @ViewChild('todoInput') todoInput: ElementRef<HTMLInputElement>;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.todos = [
+      {
+        content: "First Todo",
+        completed: false
+      },
+      {
+        content: "Second Todo",
+        completed: true
+      }
+    ]
+  }
+  
+  addTodo() {
+    this.todoInput.nativeElement.value = "";
+    this.todos.push({
+      content: this.inputTodo,
+      completed: false
+    })
+  }
+
+  toggleDone(id: number) {
+    this.todos.map((v, i) => {
+      if (i == id) v.completed = !v.completed;
+      return v;
+    })
+  }
+
+  deleteTodo(id: number) {
+    this.todos = this.todos.filter((v, i) => i !== id);
+  }
+}
